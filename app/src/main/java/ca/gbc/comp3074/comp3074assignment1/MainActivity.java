89620412a3748d2b8abb79de5b0ec5a64671e5aa
ZacShaw <zaccsshaw@me.com>
2020-09-21 16:35:09 -0400

    got 2nd activity up and working

---
+++ app/src/main/java/ca/gbc/comp3074/comp3074assignment1/MainActivity.java
@@ -0,0 +1,20 @@
+package ca.gbc.comp3074.comp3074assignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void about(View view) {
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
+    }
+}
